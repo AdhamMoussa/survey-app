d9db00f6cc77d94f3f9ca406d9439d6ef03652f5
Adham Moussa <adam.moussa62017@gmail.com>
2018-11-01 10:18:51 +0200

    finished auth flow

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const { mongoURL, cookieKey } = require("./config/keys");
+const passportConfig = require("./services/passport");
+const authRoutes = require("./routes/authRoutes");
+require("./models/User");
 
+// connect mongoose to MongoDB
+mongoose.connect(mongoURL);
+
+// init APP
 const app = express();
 
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [ cookieKey ]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passportConfig();
+authRoutes(app);
+
 const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
-  res.send({
-    hi: "there"
-  });
-});
-
 
 app.listen(PORT);
---
+++ services/passport.js
@@ -0,0 +1,50 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const { googleClientID, googleClientSecret } = require("../config/keys");
+
+module.exports = () => {
+  const User = mongoose.model("users");
+
+  // passport turns the user model to an identifier and set that as user cookie
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  // when receive request, passport turns back cookie identifier to a user model
+  passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+      done(null, user);
+    });
+  });
+
+  // init google passport strategy
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: googleClientID,
+        clientSecret: googleClientSecret,
+        callbackURL: "/auth/google/callback"
+      },
+      (accessToken, refreshToken, profile, done) => {
+        // fetch DB for that user, if exists > log in, else > creat new user
+        User.findOne({
+          googleID: profile.id
+        }).then(user => {
+          if (user) {
+            // login
+            done(null, user);
+          } else {
+            // create new user
+            new User({
+              googleID: profile.id
+            })
+              .save()
+              .then(user => {
+                done(null, user);
+              });
+          }
+        });
+      }
+    )
+  );
+};
